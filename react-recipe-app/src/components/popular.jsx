517e33e95f6ec56cfa47e141137ec3b52455062b
Bizzy-coding <bizzycoding@gmail.com>
2023-05-29 10:42:00 +0100

    looping through recipes using.map

--- react-recipe-app/src/components/popular.jsx
+++ react-recipe-app/src/components/popular.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   },[]);
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=63a977b148fe498583d565fe6ea0fed7&number=9`);
     const data = await api.json();
-    console.log(data)
+    setPopular(data.recipes)
   }
-  return <div>Popular</div>
+  return <div>
+    {popular.map((recipe) => {
+      return(
+        <div key={recipe.id}>
+          <p>{recipe.title}</p>
+        </div>
+      );
+    })};
+  </div>; 
+
 }
 export default Popular;
 
+ 

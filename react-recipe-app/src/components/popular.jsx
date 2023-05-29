845f3ee9ba5ebae3611e791280585ab6ee93cd09
Bizzy-coding <bizzycoding@gmail.com>
2023-05-29 11:13:52 +0100

    pulling in images from api & adding styled components

--- react-recipe-app/src/components/popular.jsx
+++ react-recipe-app/src/components/popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+//import {Splide, SplideSlide } from "@splidejs/react-splide";
+
 function Popular() {
 
   const [popular, setPopular] = useState([]);
@@ -10,18 +13,43 @@ function Popular() {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=63a977b148fe498583d565fe6ea0fed7&number=9`);
     const data = await api.json();
     setPopular(data.recipes)
+    console.log(data.recipes)
   }
   return <div>
     {popular.map((recipe) => {
       return(
-        <div key={recipe.id}>
-          <p>{recipe.title}</p>
-        </div>
+       <Wrapper>
+        <h3>Popular Picks</h3>
+        {popular.map((recipe) => {
+          return (
+            <Card>
+              <p>{recipe.title}</p>
+              <img src={recipe.image} alt={recipe.title} />
+            </Card>
+          );
+        })}
+        </Wrapper>
       );
     })};
   </div>; 
 
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+
+`;
+
 export default Popular;
 
  

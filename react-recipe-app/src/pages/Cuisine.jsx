7484e8be79b2510cffcb6d3698037bfc2d0765a5
Bizzy-coding <bizzycoding@gmail.com>
2023-06-25 20:05:32 +0100

    saving as everything is working - params now pull through in the react tool

--- react-recipe-app/src/pages/Cuisine.jsx
+++ react-recipe-app/src/pages/Cuisine.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Link, useParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
- 
 
 function Cuisine() {
 
-    const [cuisine, setCuisine] = useState([]);
+    const [Cuisine, setCuisine] = useState([]);
     let params = useParams();
 
     const getCuisine = async (name) => {
-        const data  = await await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=63a977b148fe498583d565fe6ea0fed7&cuisine=${name}`);
+        const data  = await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=63a977b148fe498583d565fe6ea0fed7&cuisine=${name}`);
+            
         const recipes = await data.json();
-        setCuisine(recipes.results)
-    }
+        setCuisine(recipes.results);
+
+    };
 
     useEffect(() => {
-        // getCuisine('italian')
+        getCuisine(params.type)
         console.log(params.type)
-    },[params.type]);
+    }, [params.type]);
 
-  return (
-    <div></div>
-  )
+  return <div></div>;
+  
 }
 
 export default Cuisine

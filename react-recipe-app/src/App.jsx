701bf553f6e56d38eec9d020aa73e85134b58848
Bizzy-coding <bizzycoding@gmail.com>
2023-06-25 19:23:10 +0100

    adding routing in to enable section linking

--- react-recipe-app/src/App.jsx
+++ react-recipe-app/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Category />
       <Pages/>
+    </BrowserRouter>
     </>
   )
 }

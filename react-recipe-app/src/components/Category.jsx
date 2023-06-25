6e71e68b6d1c970cf3e93882c7907a7d393dedd5
Bizzy-coding <bizzycoding@gmail.com>
2023-06-25 19:28:13 +0100

    fixed issue with cuisine spelled wrongin the category section

--- react-recipe-app/src/components/Category.jsx
+++ react-recipe-app/src/components/Category.jsx
@@ -7,19 +7,19 @@ import { NavLink } from 'react-router-dom';
 function Category() {
   return (
     <List>
-        <NavLink to={'/cusine/Italian'}>
+        <NavLink to={'/cuisine/Italian'}>
             <FaPizzaSlice />
             <h4>Italian</h4>
         </NavLink>
-        <NavLink to={'/cusine/American'}>
+        <NavLink to={'/cuisine/American'}>
             <FaHamburger />
             <h4>American</h4>
         </NavLink>
-        <NavLink to={'/cusine/Thai'}>
+        <NavLink to={'/cuisine/Thai'}>
             <GiNoodles />
             <h4>Thai</h4>
         </NavLink>
-        <NavLink to={'/cusine/Japanese'}>
+        <NavLink to={'/cuisine/Japanese'}>
             <GiChopsticks />
             <h4>Japanese</h4>
         </NavLink>

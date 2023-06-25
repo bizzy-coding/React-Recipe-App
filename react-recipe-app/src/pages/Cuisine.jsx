c95541d3cdad9ec96f949a2e0826c94912175248
Bizzy-coding <bizzycoding@gmail.com>
2023-06-25 19:45:32 +0100

    params now working for all cusines, just in the console log

--- react-recipe-app/src/pages/Cuisine.jsx
+++ react-recipe-app/src/pages/Cuisine.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+ 
 
 function Cuisine() {
+
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        const data  = await await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=63a977b148fe498583d565fe6ea0fed7&cuisine=${name}`);
+        const recipes = await data.json();
+        setCuisine(recipes.results)
+    }
+
+    useEffect(() => {
+        // getCuisine('italian')
+        console.log(params.type)
+    },[params.type]);
+
   return (
-    <div>Cuisine</div>
+    <div></div>
   )
 }
 
